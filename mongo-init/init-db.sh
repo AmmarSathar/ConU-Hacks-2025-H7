ce348ff8b35270271b828d371a5f1e904b760391
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2025-02-02 03:40:30 -0500

    Database fully mounted

--- mongo-init/init-db.sh
+++ mongo-init/init-db.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
+# Wait for MongoDB to start
+until mongosh --eval "print(\"waited for connection\")"; do
+  sleep 1
+done
 
 echo "Importing future environmental data..."
-mongoimport --db wildfire --collection future_environmental_data.csv --type csv --headerline --file /docker-entrypoint-initdb.d/future_environmental_data.csv
+mongoimport --db wildfire --collection future_environmental_data --type csv --headerline --file /docker-entrypoint-initdb.d/future_environmental_data.csv
 
 echo "Importing historical wildfire data..."
-mongoimport --db wildfire --collection historical_wildfiredata.csv --type csv --headerline --file /docker-entrypoint-initdb.d/historical_wildfiredata.csv
+mongoimport --db wildfire --collection historical_wildfiredata --type csv --headerline --file /docker-entrypoint-initdb.d/historical_wildfiredata.csv
 
-echo "Database seeding completed."
+echo "Seeding completed."
